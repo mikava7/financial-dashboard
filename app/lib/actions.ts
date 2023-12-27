c0a351dfce1842d6ba8fc7ae3bf8dc558a42f75b
mikava7 <i.mikava365@gmail.com>
2023-12-27 12:37:26 +0100

    delete invoices and cathc errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,7 @@ import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
+//Validating the types with Zod.
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string(),
@@ -21,37 +22,62 @@ export async function createInvoice(formData: FormData) {
     amount: formData.get("amount"),
     status: formData.get("status"),
   });
-
+  //Converting the amount to cents.
   const amountInCents = amount * 100;
 
   const date = new Date().toISOString().split("T")[0];
 
-  await sql`
+  try {
+    await sql`
   INSERT INTO invoices (customer_id, amount, status, date)
   VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
 `;
-
+  } catch (error) {
+    return {
+      message: "Database Error: Failed to Create Invoice.",
+    };
+  }
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
-
-  // console.log("rawFormData", amountInCents);
 }
 
 export async function updateInvoice(id: string, formData: FormData) {
+  //Extracting the data from formData.
   const { customerId, amount, status } = UpdateInvoice.parse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
   });
 
+  //  Converting the amount to cents.
   const amountInCents = amount * 100;
 
-  await sql`
+  try {
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
-
+  } catch (error) {
+    return {
+      message: "Database Error: Failed to update Invoice.",
+    };
+  }
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
+}
+
+export async function deleteInvoice(id: string) {
+  throw new Error("Failed to Delete Invoice");
+
+  try {
+    await sql`
+    DELETE FROM invoices WHERE id = ${id}`;
+
+    revalidatePath("/dashboard/invoices");
+  } catch (error) {
+    return {
+      message: "Database Error: Failed to Delete Invoice.",
+    };
+  }
 }
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,7 +9,7 @@ import {
 } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { Button } from "@/app/ui/button";
-import { UpdateInvoice } from "./buttons";
+import { updateInvoice } from "@/app/lib/actions";
 
 export default function EditInvoiceForm({
   invoice,
@@ -18,7 +18,7 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
-  const updateInvoiceWithId = UpdateInvoice.bind(null, invoice?.id);
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
 
   return (
     <form action={updateInvoiceWithId}>
